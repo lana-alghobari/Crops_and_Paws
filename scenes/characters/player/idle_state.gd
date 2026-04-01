d8e0ca54c1e43d25a1bbea708708d45e510a6de4
lana-alghobari <lana.alghobari05@gmail.com>
2026-04-01 21:40:31 +0300

    walking logic and some modification

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,41 +1,31 @@
-
 extends NodeState
 
-@export var player : CharacterBody2D
+@export var player : Player
 @export var animation : AnimatedSprite2D 
-
-var direction : Vector2
 
 func _on_process(_delta : float) -> void:
 	pass
 
 
 func _on_physics_process(_delta : float) -> void:
-	if Input.is_action_pressed("walk_left"):
-		direction = Vector2.LEFT 
-	elif Input.is_action_pressed("walk_right"):
-		direction = Vector2.RIGHT
-	elif Input.is_action_pressed("walk_up") :
-		direction = Vector2.UP
-	elif Input.is_action_pressed("walk_down"):
-		direction = Vector2.DOWN
-	else: 
-		direction = Vector2.ZERO
 	
-	if direction == Vector2.UP:
+	if player.player_direction == Vector2.UP:
 		animation.play("idle_back")
-	elif direction == Vector2.DOWN:
+	elif player.player_direction == Vector2.DOWN:
 		animation.play("idle_front")
-	elif direction == Vector2.LEFT:
+	elif player.player_direction == Vector2.LEFT:
 		animation.play("idle_left")
-	elif direction == Vector2.RIGHT:
+	elif player.player_direction == Vector2.RIGHT:
 		animation.play("idle_right")
 	else :
 		animation.play("idle_front")
 
 
 func _on_next_transitions() -> void:
-	pass
+	GameInputEvents.movement_input()
+	
+	if GameInputEvents.is_movement_input():
+		transition.emit("Walk")
 
 
 func _on_enter() -> void:
@@ -43,5 +33,5 @@ func _on_enter() -> void:
 
 
 func _on_exit() -> void:
-	pass
+	animation.stop()
  
