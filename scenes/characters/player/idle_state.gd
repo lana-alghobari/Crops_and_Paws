9692d6b583b91ab9707ef0cbf749da0efca94416
lana-alghobari <lana.alghobari05@gmail.com>
2026-04-01 19:50:36 +0300

    Idle animation according to user input

--- scenes/characters/player/idle_state.gd
+++ scenes/characters/player/idle_state.gd
@@ -1,11 +1,47 @@
-extends Node
 
+extends NodeState
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var player : CharacterBody2D
+@export var animation : AnimatedSprite2D 
 
+var direction : Vector2
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
+func _on_process(_delta : float) -> void:
 	pass
+
+
+func _on_physics_process(_delta : float) -> void:
+	if Input.is_action_pressed("walk_left"):
+		direction = Vector2.LEFT 
+	elif Input.is_action_pressed("walk_right"):
+		direction = Vector2.RIGHT
+	elif Input.is_action_pressed("walk_up") :
+		direction = Vector2.UP
+	elif Input.is_action_pressed("walk_down"):
+		direction = Vector2.DOWN
+	else: 
+		direction = Vector2.ZERO
+	
+	if direction == Vector2.UP:
+		animation.play("idle_back")
+	elif direction == Vector2.DOWN:
+		animation.play("idle_front")
+	elif direction == Vector2.LEFT:
+		animation.play("idle_left")
+	elif direction == Vector2.RIGHT:
+		animation.play("idle_right")
+	else :
+		animation.play("idle_front")
+
+
+func _on_next_transitions() -> void:
+	pass
+
+
+func _on_enter() -> void:
+	pass
+
+
+func _on_exit() -> void:
+	pass
+ 
