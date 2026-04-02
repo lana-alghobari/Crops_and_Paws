335b83f33ab9124c40e84cffe10c044adf684dbf
lana-alghobari <lana.alghobari05@gmail.com>
2026-04-02 22:01:16 +0300

    meh

--- scenes/characters/player/elstupido.gd
+++ scenes/characters/player/elstupido.gd
@@ -1,4 +1,6 @@
 class_name Player
 extends CharacterBody2D
 
+@export var current_tool 
+
 var player_direction :Vector2
---
+++ scripts/globals/data_types.gd
@@ -0,0 +1,10 @@
+class_name DataTypes
+
+enum Tools{
+	None, 
+	AxeWood,
+	TillGround,
+	WaterCorps,
+	PlantCorn,
+	PlantTomato
+}
