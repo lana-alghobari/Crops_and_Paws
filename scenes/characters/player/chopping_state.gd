26528905458d6c6320ec6c397d732550712eb1d6
lana-alghobari <lana.alghobari05@gmail.com>
2026-04-03 21:08:00 +0300

    choppie choppie

--- scenes/characters/player/chopping_state.gd
+++ scenes/characters/player/chopping_state.gd
@@ -1,5 +1,8 @@
 extends NodeState
 
+@export var player : Player 
+@export var animation : AnimatedSprite2D
+
 func _on_process(_delta : float) -> void:
 	pass
 
@@ -9,12 +12,21 @@ func _on_physics_process(_delta : float) -> void:
 
 
 func _on_next_transitions() -> void:
-	pass
+	if !animation.is_playing():
+		transition.emit("Idle")
 
 
 func _on_enter() -> void:
-	pass
-
+	if player.player_direction == Vector2.DOWN:
+		animation.play("chopping_front")
+	elif player.player_direction == Vector2.UP:
+		animation.play("chopping_back")
+	elif player.player_direction == Vector2.LEFT:
+		animation.play("chopping_left") 
+	elif player.player_direction == Vector2.RIGHT:
+		animation.play("chopping_right")
+	else:
+		animation.play("chopping_front")
 
 func _on_exit() -> void:
-	pass
+	animation.stop()
